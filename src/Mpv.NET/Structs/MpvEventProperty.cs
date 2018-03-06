fd210be150c85b93cc99cb52a4ce2e67a0e2b3a8
Aurel Hudec <hudec117@gmail.com>
2018-03-06 21:28:08 +0000

    Added ability to retrieve various data types from an event property.

--- src/Mpv.NET/Structs/MpvEventProperty.cs
+++ src/Mpv.NET/Structs/MpvEventProperty.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mpv.NET.Interop;
+using System;
 using System.Runtime.InteropServices;
 
 namespace Mpv.NET
@@ -11,5 +12,45 @@ namespace Mpv.NET
 		public MpvFormat Format;
 
 		public IntPtr Data;
+
+		public string DataString
+		{
+			get
+			{
+				CheckDataPointer();
+
+				return MpvMarshal.GetManagedUTF8StringFromPtr(Data);
+			}
+		}
+
+		public long DataLong
+		{
+			get
+			{
+				CheckDataPointer();
+
+				return Marshal.ReadInt64(Data);
+			}
+		}
+
+		public double DataDouble
+		{
+			get
+			{
+				CheckDataPointer();
+
+				var dataBytes = new byte[sizeof(double)];
+
+				Marshal.Copy(Data, dataBytes, 0, sizeof(double));
+
+				return BitConverter.ToDouble(dataBytes, 0);
+			}
+		}
+
+		private void CheckDataPointer()
+		{
+			if (Data == IntPtr.Zero)
+				throw new MpvException("Invalid data pointer.");
+		}
 	}
 }
