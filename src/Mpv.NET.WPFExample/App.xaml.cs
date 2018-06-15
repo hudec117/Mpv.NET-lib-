8a9338e32d59aa4c236ea5457d89aab385eff7ab
Aurel Hudec <hudec117@gmail.com>
2018-06-15 11:18:50 +0100

    Added WPF example project.

---
+++ src/Mpv.NET.WPFExample/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace Mpv.NET.WPFExample
+{
+	public partial class App : Application
+	{
+	}
+}
---
+++ src/Mpv.NET.WPFExample/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Mpv.NET.Player;
+using System.Windows;
+
+namespace Mpv.NET.WPFExample
+{
+	public partial class MainWindow : Window
+	{
+		private MpvPlayer player;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			player = new MpvPlayer(PlayerHost.Handle)
+			{
+				Loop = true,
+				Volume = 50
+			};
+			player.Load("http://techslides.com/demos/sample-videos/small.mp4");
+			player.Resume();
+		}
+
+		private void WindowOnClosed(object sender, System.EventArgs e)
+		{
+			player.Dispose();
+		}
+	}
+}
